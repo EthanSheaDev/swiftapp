d799edf80686cb9d486ac8bc52350219dfa25d3b
Ethan Shea <ethan.shea@estrendo.com>
2017-01-05 09:55:04 +0900

    - Text Fields added

--- swiftApp/ViewController.swift
+++ swiftApp/ViewController.swift
@@ -12,16 +12,15 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var theLabel: UILabel!
     
-    var tapCount = 0
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        tapCount = tapCount + 1
-        print(tapCount)
+        print(text1.text!)
+        print(text2.text!)
         
-        if tapCount >= 10 {
-            theLabel.text = "You tapped the button 10 times!"
-        }
     }
     
     override func viewDidLoad() {
